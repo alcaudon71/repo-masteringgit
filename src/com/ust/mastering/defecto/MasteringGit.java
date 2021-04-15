5285046d8f5910c7fb3e21e873f6912a5201f72e
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-04-15 09:23:13 +0200

    Inicio del Proyecto

---
+++ src/com/ust/mastering/defecto/MasteringGit.java
@@ -0,0 +1,16 @@
+package com.ust.mastering.defecto;
+
+public class MasteringGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Proyecto creado para MasteringGit");
+		
+		System.out.println("Curso Git - Mastering Git - Paolo Perrotta");
+		
+		System.out.println("Curso Url - https://app.pluralsight.com/course-player?clipId=671298f4-510a-4b49-8781-f12914889adf");
+		
+	}
+
+}
---
+++ src/com/ust/mastering/defecto/PaoloMaster.java
@@ -0,0 +1,12 @@
+package com.ust.mastering.defecto;
+
+public class PaoloMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Comentarios del Master Paolo");
+		
+	}
+
+}
