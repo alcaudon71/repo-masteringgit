59f3461f90ce058e7f166be795eb55c5f2e58088
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-04-19 16:42:29 +0200

    Incluir fichero GitIgnore

---
+++ src/com/ust/mastering/defecto/GitIgnoreClass.java
@@ -0,0 +1,12 @@
+package com.ust.mastering.defecto;
+
+public class GitIgnoreClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Necesito ignorar los ficheros Class del proyecto");
+		
+	}
+
+}
