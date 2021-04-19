19746fc11387856cc3e4a34d1db54ba76a457023
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-04-19 18:01:35 +0200

    Tratamiento de Ignore Class en GitHub

---
+++ src/com/ust/mastering/defecto/GithubTratamientoClass.java
@@ -0,0 +1,12 @@
+package com.ust.mastering.defecto;
+
+public class GithubTratamientoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Tratamiento de Ignore Class en GitHub");
+		
+	}
+
+}
